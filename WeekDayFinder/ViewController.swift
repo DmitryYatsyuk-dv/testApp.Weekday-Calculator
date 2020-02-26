0fa6275798d7a2012e8bfac7040b89af0d7a473b
DmitryYatsyuk-dv <yatsyuk.dev@mail.ru>
2020-02-26 23:46:27 +0300

    Complete app.Weekday Finder. Bug fixes

--- WeekDayFinder/ViewController.swift
+++ WeekDayFinder/ViewController.swift
@@ -8,9 +8,7 @@
 
 import UIKit
 
-
 class ViewController: UIViewController {
-    
     
     @IBOutlet weak var dateTF: UITextField!
     @IBOutlet weak var yearTF: UITextField!
@@ -21,7 +19,7 @@ class ViewController: UIViewController {
         let calendar = Calendar.current
         
         var dateComponents = DateComponents()
-       
+        
         // Извлечение опционалов
         guard let day = dateTF.text, let month = monthTF.text, let year = yearTF.text else {return}
         dateComponents.day = Int(day)
@@ -31,16 +29,15 @@ class ViewController: UIViewController {
         guard let date = calendar.date(from: dateComponents) else {return}
         
         let dateFormatter = DateFormatter()
-//        dateFormatter.locale = Locale(identifier: "ru_RU") // Чтобы сделать на русском языке #1
+        // dateFormatter.locale = Locale(identifier: "ru_RU") // Чтобы сделать на русском языке #1
         dateFormatter.dateFormat = "EEEE" //http://www.unicode.org/reports/tr35/tr35-31/tr35-dates.html#Date_Format_Patterns
         
         let weekday = dateFormatter.string(from: date)
-//        let capitalizedWeekday = weekday.capitalized // Чтобы сделать на русском языке #2
+        // let capitalizedWeekday = weekday.capitalized // Чтобы сделать на русском языке #2
         
         resultLabel.text = weekday
-//        resultLabel.text = capitalizedWeekday // Чтобы сделать на русском языке #3
-        }
-    
+        // resultLabel.text = capitalizedWeekday // Чтобы сделать на русском языке #3
+    }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true) // Касание экрана - убирает клавиатуру!
     }
